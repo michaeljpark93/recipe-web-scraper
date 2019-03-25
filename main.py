3a2514cd379c5833bce883fa5891fbdb0dd9f733
Michael <michaeljpark93@gmail.com>
2019-03-25 11:43:33 -0700

    Update code style and add more comments

--- main.py
+++ main.py
@@ -5,51 +5,24 @@ from time import sleep
 from random import random
 
 
-def yummily_crawler(driver, num_items):
-    domain = 'https://www.yummly.com'
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    recipes = []
-
-    # Extracts correct number of links to recipes and adds created recipe objects to array for writing to output file
-    for link in soup.findAll('a', {'class': 'card-title'}):
-        if len(recipes) < num_items:
-            recipe_link = link.get('href')
-            new_recipe = create_recipe_object(driver, domain + recipe_link)
-            recipes.append(new_recipe)
-    write_file(recipes)
+def write_file(data):
+    # Writes recipes to the output file "output.txt"
+    path = "output.txt"
+    f = open(path, "w")
+    json.dump(data, f, indent=2)
+    f.close()
 
 
-def create_recipe_object(driver, link):
-    driver.get(link)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # Creates recipe object with fields extracted from HTML
-    new_recipe = {}
-    new_recipe['recipeUrl'] = link
-    new_recipe['recipeName'] = soup.find(
-        'h1', {'class': 'recipe-title'}).get_text()
-    new_recipe['recipePhoto'] = soup.find(
-        'div', {'class': 'recipe-details-image'}).find('img').get('src')
-    new_recipe['ingredients'] = get_ingredients(
-        soup.findAll('li', {'class': 'IngredientLine'}))
-    new_recipe['ratings'] = get_ratings(
-        soup.find('a', {'class': 'recipe-details-rating'}))
-    new_recipe['cookTime'] = soup.findAll(
-        'div', {'class': 'recipe-summary-item'})[1].get_text(" ")
-    new_recipe['serve'] = int(soup.find(
-        'div', {'class': 'servings'}).find('input').get('value'))
-    return new_recipe
-
-
-def get_ratings(ratings):
+def get_ratings(ratings_element):
     # Determines how many stars are present for a given recipe, default is set to 0.0 if no ratings are given
-    count = 0.0
-    if ratings is not None:
-        for rating in ratings.children:
+    star_count = 0.0
+    if ratings_element is not None:
+        for rating in ratings_element.children:
             if rating['class'] == ['full-star']:
-                count += 1
+                star_count += 1
             elif rating['class'] == ['half-star']:
-                count += 0.5
-    return count
+                star_count += 0.5
+    return star_count
 
 
 def get_ingredients(ingredient_list):
@@ -60,30 +33,68 @@ def get_ingredients(ingredient_list):
     return ingredients
 
 
-def write_file(data):
-    # Writes recipes to the output file
-    path = "output.txt"
-    f = open(path, "w")
-    json.dump(data, f, indent=2)
-    f.close()
+def create_recipe_object(driver, recipe_link):
+    # Function visits recipe link and creates recipe object with fields extracted from page
+    driver.get(recipe_link)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    recipe = {}
+    recipe['recipeUrl'] = recipe_link
+    recipe['recipeName'] = soup.find(
+        'h1', {'class': 'recipe-title'}).get_text()
+    recipe['recipePhoto'] = soup.find(
+        'div', {'class': 'recipe-details-image'}).find('img').get('src')
+    recipe['ingredients'] = get_ingredients(
+        soup.findAll('li', {'class': 'IngredientLine'}))
+    recipe['ratings'] = get_ratings(
+        soup.find('a', {'class': 'recipe-details-rating'}))
+    recipe['cookTime'] = soup.findAll(
+        'div', {'class': 'recipe-summary-item'})[1].get_text(" ")
+    recipe['serve'] = int(soup.find(
+        'div', {'class': 'servings'}).find('input').get('value'))
+    return recipe
 
 
-def infinite_scroll(driver, num_items):
-    # Scrolls the instance of the selenium browser validate the correct number of recipes are loaded on the page
+def yummly_crawler(driver, num_recipes):
+    # Function takes Selenium browser instance of Yummly.com and creates a recipe object from each recipe link
+    domain = 'https://www.yummly.com'
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    recipes = []
+
+    # Extracts recipe information from links and adds new recipe objects to array up to specified num_recipes for writing to output file
+    recipe_links = soup.findAll('a', {'class': 'card-title'})
+    for link in recipe_links:
+        if len(recipes) < num_recipes:
+            recipe_link = link.get('href')
+            new_recipe = create_recipe_object(driver, domain + recipe_link)
+            recipes.append(new_recipe)
+    write_file(recipes)
+
+
+def scroll_down_page(driver, scroll_height, scroll_count):
+    # Uses browser scrollHeight to scroll Selenium browser instance and load more recipes
+    driver.execute_script(
+        f'const appContent = document.querySelector(\'.app-content\'); appContent.scrollTo(0, {scroll_height * scroll_count});')
+    # Mock user behavior by randomizing wait times
+    sleep(random() * 3)
+
+
+def infinite_scroll(driver, num_recipes):
+    # Scrolls the instance of the Selenium browser to validate the correct number of recipes are loaded on the page
     scroll_count = 1
     items = driver.execute_script(
         'return document.querySelectorAll(\'.recipe-card\').length;')
     scroll_height = driver.execute_script("return document.body.scrollHeight;")
-    while (items <= num_items):
-        driver.execute_script(
-            f'const appContent = document.querySelector(\'.app-content\'); appContent.scrollTo(0, {scroll_height * scroll_count});')
+    # Checks number of recipes loaded in Selenium browser instance and calls scroll_down_page if more recipes need to be loaded
+    while (items < num_recipes):
+        scroll_down_page(driver, scroll_height, scroll_count)
         items = driver.execute_script(
             'return document.querySelectorAll(\'.recipe-card\').length;')
-        sleep(random() * 3)
         scroll_count += 1
 
 
-def start(num_items=100):
+def start(num_recipes=100):
+    # Main function that gets called to run web scraper and generate recipes output file
+    # Default number of recipes is 100
     # Allows caching of resources for faster page loading
     chromeOptions = webdriver.ChromeOptions()
     prefs = {
@@ -95,12 +106,12 @@ def start(num_items=100):
     driver = webdriver.Chrome(options=chromeOptions)
     driver.implicitly_wait(15)
     driver.get('https://www.yummly.com/recipes')
-    infinite_scroll(driver, num_items)
-    yummily_crawler(driver, num_items)
+    infinite_scroll(driver, num_recipes)
+    yummly_crawler(driver, num_recipes)
     driver.quit()
 
 
-# Update NUM_ITEMS to change how many recipes are returned
+# Update NUM_RECIPES to change how many recipes are returned
 # To run web scraper execute "python3 main.py" in terminal, results will be added to output.txt file
-NUM_ITEMS = 100
-start(NUM_ITEMS)
+NUM_RECIPES = 100
+start(NUM_RECIPES)
