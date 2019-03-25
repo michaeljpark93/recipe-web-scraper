992e85443fdf8e888cd4cfcaab7b7ac0c98f9137
Michael <michaeljpark93@gmail.com>
2019-03-25 10:14:26 -0700

    Update code style and added comments

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-# imports
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import simplejson as json
-import time
+from time import sleep
+from random import random
 
 
-def yummily_crawler(driver):
+def yummily_crawler(driver, num_items):
     domain = 'https://www.yummly.com'
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     recipes = []
 
+    # Extracts correct number of links to recipes and adds created recipe objects to array for writing to output file
     for link in soup.findAll('a', {'class': 'card-title'}):
-        recipe_link = link.get('href')
-        new_recipe = create_recipe_object(domain + recipe_link)
-        recipes.append(new_recipe)
+        if len(recipes) < num_items:
+            recipe_link = link.get('href')
+            new_recipe = create_recipe_object(driver, domain + recipe_link)
+            recipes.append(new_recipe)
     write_file(recipes)
 
 
-def create_recipe_object(link):
-    source_code = requests.get(link)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, 'html.parser')
+def create_recipe_object(driver, link):
+    driver.get(link)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # Creates recipe object with fields extracted from HTML
     new_recipe = {}
     new_recipe['recipeUrl'] = link
     new_recipe['recipeName'] = soup.find(
@@ -37,16 +37,11 @@ def create_recipe_object(link):
         'div', {'class': 'recipe-summary-item'})[1].get_text(" ")
     new_recipe['serve'] = int(soup.find(
         'div', {'class': 'servings'}).find('input').get('value'))
-
-    # Bonus: add tags and recipe type to recipe item
-    new_recipe['tags'] = get_recipe_tags(
-        soup.findAll('li', {'class': 'recipe-tag'}))
-    new_recipe['recipeType'] = get_recipe_type(
-        soup.findAll('li', {'class': 'recipe-tag'}))
     return new_recipe
 
 
 def get_ratings(ratings):
+    # Determines how many stars are present for a given recipe, default is set to 0.0 if no ratings are given
     count = 0.0
     if ratings is not None:
         for rating in ratings.children:
@@ -58,56 +53,54 @@ def get_ratings(ratings):
 
 
 def get_ingredients(ingredient_list):
+    # Creates an array of ingredients with whitespaces stripped from result
     ingredients = []
     for ingredient in ingredient_list:
         ingredients.append(ingredient.get_text(" ", strip=True))
     return ingredients
 
 
-def get_recipe_tags(tag_list):
-    tags = []
-    for tag in tag_list:
-        tags.append(tag['title'])
-    return tags
-
-
-def get_recipe_type(tag_list):
-    recipeTypes = {"Main Dishes", "Beverages", "Breakfast", "Desert", "Salad"}
-    types = []
-    for tag in tag_list:
-        if tag in recipeTypes:
-            types.append(tag)
-    return types
-
-
-# Write results to the output file
 def write_file(data):
+    # Writes recipes to the output file
     path = "output.txt"
     f = open(path, "w")
     json.dump(data, f, indent=2)
     f.close()
 
-# Scrolls the instance of the selenium browser to maximize the number of recipes that are loaded on the page for scraping
-def infinite_scroll(driver, num_items):
-    count = 1
-    items = 0
-    last_height = driver.execute_script("return document.body.scrollHeight;")
-    while (items <= num_items):
-        driver.execute_script(f'const appContent = document.querySelector(\'.app-content\'); appContent.scrollTo(0, {last_height * count});')
-        items = driver.execute_script('return document.querySelectorAll(\'.recipe-card\').length;')
-        time.sleep(3)
-        count += 1
-    time.sleep(10)
 
-# Start function runs an instance of a browser through selenium and executes the inifite scroll function to ensure the correct number of results are loaded
-def start(num_items = 100):
-    driver = webdriver.Chrome()
+def infinite_scroll(driver, num_items):
+    # Scrolls the instance of the selenium browser validate the correct number of recipes are loaded on the page
+    scroll_count = 1
+    items = driver.execute_script(
+        'return document.querySelectorAll(\'.recipe-card\').length;')
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")
+    while (items <= num_items):
+        driver.execute_script(
+            f'const appContent = document.querySelector(\'.app-content\'); appContent.scrollTo(0, {scroll_height * scroll_count});')
+        items = driver.execute_script(
+            'return document.querySelectorAll(\'.recipe-card\').length;')
+        sleep(random() * 3)
+        scroll_count += 1
+
+
+def start(num_items=100):
+    # Allows caching of resources for faster page loading
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {
+        'disk-cache-size': 4096
+    }
+    chromeOptions.add_experimental_option('prefs', prefs)
+
+    # Runs an instance of a browser through selenium and executes scrolling function to ensure the correct number of results are loaded
+    driver = webdriver.Chrome(options=chromeOptions)
+    driver.implicitly_wait(15)
     driver.get('https://www.yummly.com/recipes')
-    driver.implicitly_wait(50)
     infinite_scroll(driver, num_items)
-    yummily_crawler(driver)
+    yummily_crawler(driver, num_items)
     driver.quit()
 
-# update NUM_ITEMS to change how many results are returned in the output.txt file
+
+# Update NUM_ITEMS to change how many recipes are returned
+# To run web scraper execute "python3 main.py" in terminal, results will be added to output.txt file
 NUM_ITEMS = 100
 start(NUM_ITEMS)
